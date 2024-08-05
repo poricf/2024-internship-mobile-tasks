205856fffe279d210bd2cd4dc3fde72b63fbcb3d
poricf <famkhan006@gmail.com>
2024-08-05 09:41:14 +0300

    implemeted the domain layer

--- on-boarding/ecommerce/lib/features/product/Domain/entities/product.dart
+++ on-boarding/ecommerce/lib/features/product/Domain/entities/product.dart
@@ -1,15 +1,17 @@
-class Product {
-  final String id;
-  final String name;
-  final String description;
-  final String imageUrl;
-  final double price;
+import 'package:equatable/equatable.dart';
 
-  Product({
-    required this.id,
-    required this.name,
-    required this.description,
-    required this.imageUrl,
-    required this.price,
-  });
+class Product extends Equatable {
+  final int? productid;
+  final String? name;
+  final String? description;
+  final double? price;
+  final String? imageUrl;
+  const Product(
+      {this.productid, this.name, this.description, this.price, this.imageUrl});
+
+  @override
+  // TODO: implement props
+  List<Object?> get props {
+    return [productid, name, description, price, imageUrl];
+  }
 }
